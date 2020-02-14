df6ee31f69456cb1b40ba964526b90dba3d4171e
Andoni Teso  García <andoniteso@gmail.com>
2020-02-14 17:26:02 +0100

    Add files via upload

---
+++ src/dataMining/Main.java
@@ -0,0 +1,74 @@
+package dataMining;
+
+import java.util.Random;
+
+import librerias.Utilidades;
+import weka.classifiers.Evaluation;
+import weka.classifiers.lazy.IBk;
+import weka.core.Instances;
+import weka.core.SerializationHelper;
+
+//arg 1: Path del fichero de datos
+//arg 2: /Path para guardar el txt con los resultados.
+//arg 3: Path para guardar el modelo (.model).
+//arg 4: Path para guardar el txt con las predicciones.
+
+public class Main {
+	
+	private static double porcentajeSplit = 25.0;
+
+	public static void main(String[] args) throws Exception {
+
+		if (args.length != 4)
+			System.out.println("ERROR: Se necesitan 4 argumentos de entrada: " 
+					+ "\n\t*1: Path del fichero de datos."
+					+ "\n\t*2: Path para guardar el txt con los resultados."
+					+ "\n\t*3: Path para guardar el modelo (.model)."
+					+ "\n\t*4: Path para guardar el txt con las predicciones.");
+		else {
+			/* 1. Cargar los datos desde el arff */
+			Instances data = Utilidades.getUtilidades().cargarDatos(args[0]);
+
+			/* 2. Seleccionar la clase, randomizar y, si fuera necesario, filtrar */
+			if (data.attribute("class") == null)
+				data.setClassIndex(data.numAttributes() - 1);
+			else
+				data.setClass(data.attribute("class"));
+			Instances newData = Utilidades.getUtilidades().filtrar(data);
+			
+			newData.randomize(new Random(1));
+			int insTrain = (int) (newData.size() * (porcentajeSplit / 100)); // Número de instancias para el train
+			int insTest = newData.size() - insTrain; // Número de instancias para el test
+			Instances train = new Instances(newData, 0, insTrain - 1);
+			Instances test = new Instances(newData, insTrain, insTest - 1);
+
+			/* 3. Inicializar clasificador kNN */
+			IBk clasificador = Utilidades.getUtilidades().configurarIBk(newData);
+			//clasificador.buildClassifier(newData);
+			
+			/* 4. Hacer la evaluación */
+			/* 4.1. CroosValidation */
+			Evaluation evaluator = Utilidades.getUtilidades().crossValidation(clasificador, newData, 10, args[2]);
+			
+
+			/* 5. Mostrar y guardar resultados */
+			String resultados = "\n=== Summary ===\n" + evaluator.toSummaryString() + "\n" + "Recall Class 0: "
+					+ evaluator.recall(0) + "\n" + "Weighted Average Recall: " + evaluator.weightedRecall() + "\n"
+					+ "\n" + evaluator.toClassDetailsString() + "\n" + evaluator.toMatrixString();
+			System.out.println(resultados);
+			Utilidades.getUtilidades().guardarResultados(resultados, args[1]);
+
+			/* 6. Cargar modelo y hacer predicciones */
+			IBk clasificadorModelo = (IBk) SerializationHelper.read(args[2]);
+			String predicciones = "=== Predicctions ===\n \ninst# --> instance atributes --> predicted" + "\n";
+			Instances dataPred = Utilidades.getUtilidades().filtrar(data); // Para que este filtrado
+			for (int i = 0; i < dataPred.numInstances(); i++) {
+				double pred = clasificadorModelo.classifyInstance(dataPred.instance(i));
+				predicciones += "\n" + i + " --> " + data.instance(i);
+				predicciones += " --> " + dataPred.classAttribute().value((int) pred);
+			}
+			System.out.println(predicciones);
+			Utilidades.getUtilidades().guardarResultados(predicciones, args[3]);
+		}
+	}
+}
---
+++ src/librerias/Utilidades.java
@@ -0,0 +1,240 @@
+package librerias;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+import weka.attributeSelection.BestFirst;
+import weka.attributeSelection.CfsSubsetEval;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.lazy.IBk;
+import weka.classifiers.meta.CVParameterSelection;
+import weka.core.Attribute;
+import weka.core.AttributeStats;
+import weka.core.ChebyshevDistance;
+import weka.core.EuclideanDistance;
+import weka.core.Instances;
+import weka.core.ManhattanDistance;
+import weka.core.SelectedTag;
+import weka.core.SerializationHelper;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.neighboursearch.LinearNNSearch;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+
+public class Utilidades {
+
+	private static Utilidades mUtilidades;
+
+	public static Utilidades getUtilidades() {
+		if (mUtilidades == null)
+			mUtilidades = new Utilidades();
+		return mUtilidades;
+	}
+
+	public Instances cargarDatos(String pFichero) throws Exception {
+
+		// Leemos el fichero
+		DataSource source = null;
+		try {
+			source = new DataSource(pFichero);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("ERROR: el archivo no se ha encontrado");
+		}
+
+		// Cargamos las instancias
+		Instances data = source.getDataSet();
+
+		return data;
+	}
+
+	public void guardarResultados(String pResultado, String pFichero) {
+		try {
+			PrintWriter out = new PrintWriter(pFichero);
+			out.println(pResultado);
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Instances filtrar(Instances pData) throws Exception {
+		AttributeSelection filter = new AttributeSelection();
+		CfsSubsetEval eval = new CfsSubsetEval();
+		BestFirst search = new BestFirst();
+
+		filter.setEvaluator(eval);
+		filter.setSearch(search);
+		filter.setInputFormat(pData);
+		Instances newData = Filter.useFilter(pData, filter);
+
+		return newData;
+	}
+
+	public Evaluation crossValidation(Classifier pClasificador, Instances pData, int pFolds, String pFichero)
+			throws Exception {
+		pClasificador.buildClassifier(pData);
+		SerializationHelper.write(pFichero, pClasificador);
+		Evaluation evaluator = new Evaluation(pData);
+		evaluator.crossValidateModel(pClasificador, pData, pFolds, new Random(1));
+		return evaluator;
+	}
+
+	public Evaluation holdOut(Classifier pClasificador, Instances pTrain, Instances pTest) throws Exception {
+		pClasificador.buildClassifier(pTrain);
+		SerializationHelper.write("modelo.model", pClasificador);
+		Evaluation evaluation = new Evaluation(pTrain);
+		evaluation.evaluateModel(pClasificador, pTest);
+		return evaluation;
+	}
+
+	public Evaluation noHonesta(Classifier pClasificador, Instances pData) throws Exception {
+		pClasificador.buildClassifier(pData);
+		SerializationHelper.write("modelo.model", pClasificador);
+		Evaluation evaluation = new Evaluation(pData);
+		evaluation.evaluateModel(pClasificador, pData);
+		return evaluation;
+	}
+
+	public IBk configurarIBk(Instances pData) throws Exception {
+		int bestK = 1;
+		int bestDistance = 1;
+		int bestWeight = 1;
+		double bestF = 0;
+		int minClassIndex = getIndClaseMinoritaria(pData);
+		IBk cls;
+		Evaluation eval;
+		
+		System.out.println("Searching best parameters...\n");
+
+		for (int k = 1; k <= pData.numInstances() * 0.4; k++) {
+			for (int w = 1; w <= 3; w++) {
+				for (int d = 1; d <= 3; d++) {
+					// Manhattan con WEIGHT_SIMILARITY casca, por lo que nos lo saltamos
+					if (d != 1 || w != 2) {
+						cls = getKNN(pData, k, d, w);
+						eval = new Evaluation(pData);
+						eval.crossValidateModel(cls, pData, 10, new Random(1));
+						if (eval.fMeasure(minClassIndex) > bestF) {
+							bestK = k;
+							bestDistance = d;
+							bestWeight = w;
+							bestF = eval.fMeasure(minClassIndex);
+						}
+					}
+				}
+			}
+		}
+		
+		cls = getKNN(pData, bestK, bestDistance, bestWeight);
+		System.out.println("################################");
+		System.out.println("BEST IBk PARAMETERS:");
+		System.out.println("KNN: " + bestK);
+		switch (bestDistance) {
+		case 1:
+			System.out.println("Distance function: Manhattan Distance");
+			break;
+		case 2:
+			System.out.println("Distance function: Euclidean Distance");
+			break;
+		case 3:
+			System.out.println("Distance function: Chebyshev Distance");
+			break;
+		default:
+			break;
+		}
+		switch (bestWeight) {
+		case 1:
+			System.out.println("Distance weighting: Weight by 1/distance");
+			break;
+		case 2:
+			System.out.println("Distance weighting: No distance weighting");
+			break;
+		case 3:
+			System.out.println("Distance weighting: Weight by 1 - distance");
+			break;
+		default:
+			break;
+		}
+		System.out.println("################################");
+		return cls;
+	}
+
+	private IBk getKNN(Instances data, int k, int distance, int weight) throws Exception {
+
+		IBk estimador = new IBk(k);
+		LinearNNSearch search = new LinearNNSearch();
+
+		switch (distance) {
+		case 1:
+			search.setDistanceFunction(new ManhattanDistance());
+			break;
+		case 2:
+			search.setDistanceFunction(new EuclideanDistance());
+			break;
+		case 3:
+			search.setDistanceFunction(new ChebyshevDistance());
+			break;
+		}
+
+		switch (weight) {
+		case 1:
+			estimador.setDistanceWeighting(new SelectedTag(IBk.WEIGHT_INVERSE, IBk.TAGS_WEIGHTING));
+			break;
+		case 2:
+			estimador.setDistanceWeighting(new SelectedTag(IBk.WEIGHT_SIMILARITY, IBk.TAGS_WEIGHTING));
+			break;
+		case 3:
+			estimador.setDistanceWeighting(new SelectedTag(IBk.WEIGHT_NONE, IBk.TAGS_WEIGHTING));
+			break;
+		}
+		return estimador;
+	}
+	
+	private int getIndClaseMinoritaria(Instances pData) {
+		int ind = pData.classIndex();
+		AttributeStats stats = pData.attributeStats(ind);
+		int[] frecuencias = stats.nominalCounts;
+		int frecMin = frecuencias[0];
+		int index = 0;
+		int res = 0;
+		for (int f: frecuencias) {
+			if (f < frecMin && f != 0) {
+				frecMin = f;
+				res = index;
+			}
+			index++;
+		}
+		return res;
+	}
+	
+	private double getFMeasure(Evaluation pEval, Instances pData) {
+		Attribute classA = pData.classAttribute();
+
+		// Lista con las fMeasures de cada clase
+		double[] fMeasures = new double[classA.numValues()];
+		for (int i = 0; i < classA.numValues(); i++) {
+			fMeasures[i] = pEval.fMeasure(i);
+			if (Double.isNaN(fMeasures[i])) {
+				// Todo es 0
+				fMeasures[i] = 0;
+			}
+		}
+
+		// Lista con el peso de cada clase
+		int[] weights = new int[classA.numValues()];
+		for (int j = 0; j < pData.numInstances(); j++)
+			weights[(int) pData.instance(j).classValue()]++;
+
+		// fMeasure es la media del array
+		int fMeasure = 0;
+		for (int k = 0; k < classA.numValues(); k++)
+			fMeasure += weights[k] * fMeasures[k];
+
+		fMeasure = fMeasure / pData.numInstances();
+		return fMeasure;
+	}
+
+}
